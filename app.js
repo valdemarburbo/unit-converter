8f7fa36612ff8ef17e73ed134ab52780143b36f8
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-09-05 20:19:09 +0100

    Add mass equation. Correct unit ratios.

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const volumeEl = document.getElementById("volume-el")
 const massEl = document.getElementById("mass-el")
 
 convertBtn.addEventListener("click", function () {
-    lengthEl.innerHTML = `${inputNum.value} meters = ${(inputNum.value * 3.281).toFixed(3)} feet | ${inputNum.value} feet = ${(inputNum.value / 3.281).toFixed(3)} meters`
+    lengthEl.innerHTML = `${inputNum.value} meters = ${(inputNum.value * 3.28084).toFixed(3)} feet | ${inputNum.value} feet = ${(inputNum.value / 3.28084).toFixed(3)} meters`
 
-    volumeEl.innerHTML = `${inputNum.value} liters = ${(inputNum.value * 0.264).toFixed(3)} gallons | ${inputNum.value} gallons = ${(inputNum.value / 0.264).toFixed(3)} liters`
+    volumeEl.innerHTML = `${inputNum.value} liters = ${(inputNum.value * 0.219969).toFixed(3)} gallons | ${inputNum.value} gallons = ${(inputNum.value / 0.219969).toFixed(3)} liters`
+
+    massEl.innerHTML = `${inputNum.value} kilos = ${(inputNum.value * 2.20462).toFixed(3)} pounds | ${inputNum.value} pounds = ${(inputNum.value / 2.20462).toFixed(3)} kilos`
 })
 
 
